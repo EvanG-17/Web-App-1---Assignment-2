0ad988ce0f99f6921533a34fe2914e9c3f179760
Glitch (polite-large-butterfly) <none>
2022-05-01 18:13:38 +0000

    🐪👀 Checkpoint ./server.js:41234484/1572 ./views/partials/listmovielists.hbs:41234484/21 ./views/partials/listmovies.hbs:41234484/683

--- server.js
+++ server.js
@@ -22,6 +22,36 @@ app.use(fileUpload());
 app.engine('.hbs', exphbs({
   extname: '.hbs',
   defaultLayout: 'main',
+    helpers: {
+          uppercase: (inputString) => {
+        return inputString.toUpperCase();
+    },
+            formatDate: (date) =>  {
+                 let dateCreated = new Date(date);
+        let dateNum = dateCreated.getDate();
+        let month = dateCreated.getMonth();
+        let year = dateCreated.getFullYear();
+
+        let months = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ];
+        let monthname = months[month];
+        return `${monthname} ${dateNum}, ${year}`;
+      },
+
+
+  }
 }));
 app.set('view engine', '.hbs');
 
@@ -33,3 +63,4 @@ app.use('/', routes);
 const listener = app.listen(process.env.PORT || 4000, function () {
   logger.info('Your app is listening on port ' + listener.address().port);
 });
+
